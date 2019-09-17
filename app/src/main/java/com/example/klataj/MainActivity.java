b36cd4526b8e4bcb1ea3461d4224c2fc3fc3e0e5
darrylpierre <darryl9623@gmail.com>
2019-09-17 17:59:02 -0400

    Initial commit

---
+++ app/src/main/java/com/example/klataj/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.klataj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+
+        if (currentUser == null){
+
+            Intent startIntent = new Intent(MainActivity.this,StartActivity.class);
+            startActivity(startIntent);
+            finish();
+        }
+    }
+}
