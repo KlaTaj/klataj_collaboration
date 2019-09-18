c896bc876bb86c6d9bc1cdafda5813e9c34cab88
darrylpierre <darryl9623@gmail.com>
2019-09-18 03:28:41 -0400

    gitignore

--- app/src/main/java/com/example/klataj/RegisterActivity.java
+++ app/src/main/java/com/example/klataj/RegisterActivity.java
@@ -3,12 +3,42 @@ package com.example.klataj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText pseudo, imel,modpas;
+    TextView txt_register, txt_signup;
+    Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // findViewById for editText
+        pseudo = (EditText) findViewById(R.id.etPseudo);
+        imel = (EditText) findViewById(R.id.email_register);
+        modpas = (EditText) findViewById(R.id.pwrd_register);
+
+        // findViewById for textView
+        txt_register = (TextView) findViewById(R.id.txt_register);
+        txt_signup = (TextView) findViewById(R.id.txt_signup);
+
+        // findViewById for button
+        btn_register = (Button) findViewById(R.id.btn_register);
+
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/klataj/StartActivity.java
+++ app/src/main/java/com/example/klataj/StartActivity.java
@@ -2,13 +2,29 @@ package com.example.klataj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+
+    private Button btn_create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        btn_create = (Button) findViewById(R.id.btn_create);
+        btn_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent register_intent = new Intent(StartActivity.this,RegisterActivity.class);
+                startActivity(register_intent);
+                finish();
+            }
+        });
     }
 }
