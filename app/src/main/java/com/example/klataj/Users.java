1ef523e4abee5dbdc4e677e2b85618d1c7f519ae
darrylpierre <darryl9623@gmail.com>
2019-10-01 18:15:04 -0400

    get and set Users

--- app/src/main/java/com/example/klataj/Users.java
+++ app/src/main/java/com/example/klataj/Users.java
@@ -1,4 +1,32 @@
 package com.example.klataj;
 
 public class Users {
+
+    public String name;
+    public String image;
+    public String status;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
