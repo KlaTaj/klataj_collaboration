2039fd2749eca2088182d773038fe7b07103c31c
darrylpierre <darryl9623@gmail.com>
2019-10-01 03:14:36 -0400

    users

--- app/src/main/java/com/example/klataj/UsersActivity.java
+++ app/src/main/java/com/example/klataj/UsersActivity.java
@@ -1,14 +1,28 @@
 package com.example.klataj;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class UsersActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
+
+        toolbar = (Toolbar) findViewById(R.id.users_appBar);
+        getSupportActionBar().setTitle("Tout itilizatè");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setSupportActionBar(toolbar);
+
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.users_list);
+
     }
 }
